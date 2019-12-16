51bad8dc1a6d9a1e918bd4dd63187ec7dea6ab5e
jackgilbertx <jackgilbertx@gmail.com>
2019-12-16 15:42:54 -0600

    changes

--- src/FollowButton.js
+++ src/FollowButton.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { TwitterFollowButton } from "react-twitter-embed";
+// import { TwitterFollowButton } from "react-twitter-embed";
 import "./styles.css";
 
 const FollowButton = ({ renderFollowButton }) => {
-  const language = window.navigator.language;
+  // const language = window.navigator.language;
 
   return (
     <div>
       {renderFollowButton && (
         <div className="follow">
-          <TwitterFollowButton
+          {/* <TwitterFollowButton
             screenName="cargill"
             options={{ lang: language, showCount: "false", size: "large" }}
-          />
-          {/* <button
+          /> */}
+          <button
             type="button"
             class="btnColor"
             onClick={() => window.open("https://www.google.com")}
           >
+            <img src="Twitter_Logo.png" alt="pic" />
             Follow Us
-          </button> */}
+          </button>
         </div>
       )}
     </div>
